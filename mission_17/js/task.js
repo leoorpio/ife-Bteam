ae580ac73bf6770b01b68d9e66a05029aa4180a7
leoorpio <756848679@qq.com>
2016-04-22 14:07:08 +0800

    Update

--- mission_17/js/task.js
+++ mission_17/js/task.js
@@ -9,27 +9,54 @@ var aqiSourceData = {
 };
 */
 
-// 以下两个函数用于随机模拟生成测试数据
-function getDateStr(dat) {
-  var y = dat.getFullYear();
-  var m = dat.getMonth() + 1;
-  m = m < 10 ? '0' + m : m;
-  var d = dat.getDate();
-  d = d < 10 ? '0' + d : d;
-  return y + '-' + m + '-' + d;
-}
-function randomBuildData(seed) {
-  var returnData = {};
-  var dat = new Date("2016-01-01");
-  var datStr = ''
-  for (var i = 1; i < 92; i++) {
-    datStr = getDateStr(dat);
-    returnData[datStr] = Math.ceil(Math.random() * seed);
-    dat.setDate(dat.getDate() + 1);
+var EventUtil = {
+  addHandler: function(element, type, handler) {
+    if(element.addEventListener) {
+      element.addEventListener(type, handler, false);
+    }else if(element.attachEvent) {
+      element.attachEvent("on"+type, handler);
+    }else {
+      element["on" + type] = handler;
+    }
+  },
+
+  getEvent: function(event) {
+    return event ? event : window.event;
+  },
+  getTarget: function(event) {
+    return event.target || event.srcElement;
   }
-  return returnData;
 }
 
+// 以下两个函数用于随机模拟生成测试数据
+function getDateStr(dat) {
+    var y = dat.getFullYear();      // 取得四位数的年份
+    var m = dat.getMonth() + 1;     // 取得日期中的月份，其中0表示一月
+    m = m < 10 ? '0' + m : m;       // 如果月份是小于10，个位数前面补“0”
+    var d = dat.getDate();          // 返回日期月份中的天数（1到31）
+    d = d < 10 ? '0' + d : d;       // 如果天数小于10，个位数前面补“0”
+    return y + '-' + m + '-' + d;   // 返回字符串日期
+}
+/**
+ * [randomBuildData 随机数据]
+ * @param  {integer} seed  
+ * @return {object} 
+ * Math.random()返回大于等于0小于1的一个随机数。   
+ */
+function randomBuildData(seed) {
+    var returnData = {};        // 创建一个存储日期的对象
+    var dat = new Date("2016-01-01");   // 设置日期
+    var datStr = '';                // 创建一个字符串类型 datStr
+    for (var i = 1; i < 92; i++) {  // 循环91次  一月31天 2月29天  三月31天
+        datStr = getDateStr(dat);
+        returnData[datStr] = Math.ceil(Math.random() * seed);   // 小数值舍入为正数的方法Math.ceil():执行向上舍入，即它总是向上舍入为最接近的整数;
+        dat.setDate(dat.getDate() + 1);   // 日期月份中的天数+1
+    }
+    return returnData;
+}
+
+// aqiSourceData对象中每个key都是一个returnData对象，保存有91天的数值。
+var date1 = new Date();
 var aqiSourceData = {
   "北京": randomBuildData(500),
   "上海": randomBuildData(300),
@@ -41,6 +68,9 @@ var aqiSourceData = {
   "厦门": randomBuildData(100),
   "沈阳": randomBuildData(500)
 };
+var date2 = new Date();
+console.log(date2-date1);
+console.log(aqiSourceData);
 
 // 用于渲染图表的数据
 var chartData = {};
@@ -61,9 +91,10 @@ function renderChart() {
 /**
  * 日、周、月的radio事件点击时的处理函数
  */
-function graTimeChange() {
+function graTimeChange(event) {
   // 确定是否选项发生了变化 
-
+  console.log('touch me');
+  console.log(event);
   // 设置对应数据
 
   // 调用图表渲染函数
@@ -84,6 +115,10 @@ function citySelectChange() {
  * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
  */
 function initGraTimeForm() {
+  var radio = document.getElementsByName('gra-time');
+  for(var i = 0, len = radio.length; i < len; i++) {
+    EventUtil.addHandler(radio[i], 'click', graTimeChange);
+  }
 
 }
 
